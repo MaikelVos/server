21f8e04acbe4df99af82a29de675e07c83f2bb37
maikelvos <mvos20@avans.nl>
2018-04-25 14:41:57 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const port = 8080 || process.env.PORT;
+
+const app = express();
+
+app.get('/hello' , (req, res) => {
+    res.json({'msg': 'hello you all'})
+
+});
+
+app.listen(port, () => {
+    console.log('server is ready ')
+});
